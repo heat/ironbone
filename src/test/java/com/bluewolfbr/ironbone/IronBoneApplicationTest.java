9955508741254fdec03de7cdcdbcf5500a096dc5
Onezino Gabriel Moreira <zinogabriel@gmail.com>
2013-02-23 23:19:39 -0300

    add license header

--- src/test/java/com/bluewolfbr/ironbone/IronBoneApplicationTest.java
+++ src/test/java/com/bluewolfbr/ironbone/IronBoneApplicationTest.java
@@ -1,8 +1,20 @@
+/*
+ * Copyright 2013 OnezinoGabriel.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.bluewolfbr.ironbone;
 
-import com.bluewolfbr.ironbone.Column;
-import com.bluewolfbr.ironbone.Table;
-import com.bluewolfbr.ironbone.IronBoneApplication;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -13,7 +25,6 @@ import java.net.URL;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
@@ -22,6 +33,8 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 import com.bluewolfbr.ironbone.Column.COLUMN_TYPE;
+import java.io.FileNotFoundException;
+import java.net.MalformedURLException;
 import org.junit.Test;
 
 public class IronBoneApplicationTest {
@@ -30,7 +43,8 @@ public class IronBoneApplicationTest {
     private static Connection conn;
 
     @org.junit.BeforeClass
-    public static void setUp() throws ClassNotFoundException, SQLException {
+    public static void setUp() throws ClassNotFoundException, SQLException, MalformedURLException, URISyntaxException, FileNotFoundException {
+        
         Class.forName("org.hsqldb.jdbcDriver");
         String url = "jdbc:hsqldb:mem:data/tutorial";
 
